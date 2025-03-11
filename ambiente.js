090ab86689b5b2481bd9c7b46e2fe01859d25792
V. Akio Isawa <163936837+Akio60@users.noreply.github.com>
2025-03-11 15:00:51 -0300

    Create ambiente.js

---
+++ ambiente.js
@@ -0,0 +1,16 @@
+public class Ambiente {
+    // Atributos do ambiente
+    private int largura;
+    private int altura;
+
+    // Construtor para inicializar os atributos do ambiente
+    public Ambiente(int largura, int altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    // Método que verifica se uma determinada posição (x, y) está dentro dos limites do ambiente
+    public boolean dentroDosLimites(int x, int y) {
+        return (x >= 0 && x <= largura) && (y >= 0 && y <= altura);
+    }
+}
