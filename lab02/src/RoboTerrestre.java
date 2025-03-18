add125c91df91c0377d8ab16c82402b1b34888c9
V. Akio Isawa <vitorakioisawa@gmail.com>
2025-03-18 14:41:52 -0300

    correção das pastas

---
+++ lab02/src/RoboTerrestre.java
@@ -0,0 +1,21 @@
+public class RoboTerrestre extends Robo {
+    int velocidadeMaxima;
+
+    public RoboTerrestre(String nome,
+                         String direcao, 
+                         int posicaoX, 
+                         int posicaoY, 
+                         int velocidadeMaxima) {
+        super(nome, direcao, posicaoX, posicaoY);
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    @Override
+    public void mover(int deltaX, int deltaY) {
+        if (Math.abs(deltaX) <= velocidadeMaxima && Math.abs(deltaY) <= velocidadeMaxima) {
+            super.mover(deltaX, deltaY);
+        } else {
+            System.out.println("Movimento excede velocidade máxima!");
+        }
+    }
+}
